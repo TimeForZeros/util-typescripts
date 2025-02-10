013136a74f8b15fbdad3b8ba51bc38451af0562b
Victor Gonzalez <dazedbutamused@gmail.com>
2025-02-10 14:33:44 -0800

    rename for better understanding

--- src/filesystem/compare-handbrake-output.ts
+++ src/filesystem/compare-handbrake-output.ts
@@ -10,13 +10,13 @@ const compareHandbrakeOutput = async (dir: string, options: { outputDir: string 
   const outputFiles = files.filter((file) => file.endsWith('_AV1.mp4') || file.endsWith('_H.265.mp4'));
   for (const outputFilePath of outputFiles) {
     const originalFilePattern = path.basename(outputFilePath).replace(/(_AV1|_H\.265).mp4/, '');
-    const foundPath = files.find((file: string) => file.includes(originalFilePattern) && !file.endsWith('_AV1.mp4') && !file.endsWith('_H.265.mp4'));
-    if (!foundPath) continue;
+    const originalPath = files.find((file: string) => file.includes(originalFilePattern) && !file.endsWith('_AV1.mp4') && !file.endsWith('_H.265.mp4'));
+    if (!originalPath) continue;
     const outputSize = (await fs.stat(outputFilePath)).size;
-    const originalSize = (await fs.stat(foundPath)).size;
+    const originalSize = (await fs.stat(originalPath)).size;
     if (outputSize < originalSize) {
-      console.log(`Removing larger file: ${foundPath}`);
-      await fs.remove(foundPath);
+      console.log(`Removing larger file: ${originalPath}`);
+      await fs.remove(originalPath);
     } else {
       console.log(`Removing larger file: ${outputFilePath}`);
       await fs.remove(outputFilePath);
