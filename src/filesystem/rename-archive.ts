1ba36ca26808ce7028e1aa6115e7f16ba1f07592
Victor Gonzalez <dazedbutamused@gmail.com>
2025-05-28 10:32:38 -0700

    undo archival naming

--- src/filesystem/rename-archive.ts
+++ src/filesystem/rename-archive.ts
@@ -2,18 +2,28 @@ import path from 'path';
 import fs from 'fs-extra';
 import { program } from 'commander';
 
-const renameArchiveDir = async (dir: string) => {
+const renameArchiveDir = async (dir: string, { undo = false }) => {
   const files = await fs.readdir(dir);
   const dirPromises = files.map(async (file) => {
     const stat = await fs.stat(path.join(dir, file));
     if (!stat.isDirectory()) return;
     const sourcePath = path.join(dir, file);
-    const destinationName = `${file.toLowerCase().replaceAll(' ', '-')}_originals`;
+    const destinationName = undo
+      ? file
+          .replace('_originals', '')
+          .split('-')
+          .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+          .join(' ')
+      : `${file.toLowerCase().replaceAll(' ', '-')}_originals`;
     const destinationPath = path.join(dir, destinationName);
-    console.log(`renaming: ${file} -> ${destinationName}`)
+    console.log(`renaming: ${file} -> ${destinationName}`);
     await fs.rename(sourcePath, destinationPath);
   });
   await Promise.allSettled(dirPromises);
 };
 
-program.argument('dir', 'directory to search').action(renameArchiveDir).parse();
+program
+  .argument('dir', 'directory to search')
+  .option('-u, --undo', 'undo-archival name')
+  .action(renameArchiveDir)
+  .parse();
